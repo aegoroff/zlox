8106b9908e25e2d3979e082636ad3049d860a179
Alexander Egorov <egoroff@gmail.com>
2026-06-25 07:07:43 +0300

    feat: nice values formatting implemented

--- src/value.zig
+++ src/value.zig
@@ -1,4 +1,13 @@
+const std = @import("std");
+
 pub const LoxValue = union(enum) {
     Number: f64,
     Bool: bool,
+
+    pub fn format(self: LoxValue, writer: *std.Io.Writer) !void {
+        switch (self) {
+            .Number => |n| try writer.print("{d}", .{n}),
+            .Bool => |b| try writer.print("{}", .{b}),
+        }
+    }
 };
