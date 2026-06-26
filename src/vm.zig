aeccc98ed7c0a5d7ea8dc017a2418491dfe77150
Alexander Egorov <egoroff@gmail.com>
2026-06-26 07:13:13 +0300

    refactor: ip made local var of run

--- src/vm.zig
+++ src/vm.zig
@@ -6,13 +6,11 @@ const err = @import("error.zig");
 
 allocator: std.mem.Allocator,
 chunk: *chk.Chunk,
-ip: *std.ArrayList(u8),
 
 pub fn init(gpa: std.mem.Allocator) VM {
     return VM{
         .allocator = gpa,
         .chunk = undefined,
-        .ip = undefined,
     };
 }
 
@@ -20,13 +18,14 @@ pub fn deinit(_: *VM) void {}
 
 pub fn interpret(self: *VM, chunk: *chk.Chunk) err.Error!void {
     self.chunk = chunk;
-    self.ip = &chunk.code;
     try self.run();
 }
 
 pub fn run(self: *VM) err.Error!void {
-    for (0..self.ip.items.len) |offset| {
-        const opcode = self.chunk.readOpcode(offset);
+    var ip: usize = 0;
+    while (ip < self.chunk.code.items.len) {
+        const opcode = self.chunk.readOpcode(ip);
+        ip += 1;
         switch (opcode) {
             chk.OpCode.Return => return,
             else => {},
