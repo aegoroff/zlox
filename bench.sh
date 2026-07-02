27a45a453357d736a8ca96d2fb76234aea23019f
Alexander Egorov <egoroff@gmail.com>
2026-07-02 22:40:26 +0300

    chore: clox set as zero benchmarking point

--- bench.sh
+++ bench.sh
@@ -4,18 +4,18 @@
 # Usage: ./bench.sh <lox_file>
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <lox_file>"
-    exit 1
+  echo "Usage: $0 <lox_file>"
+  exit 1
 fi
 
 LOX_FILE="$1"
 
 if [ ! -f "$LOX_FILE" ]; then
-    echo "Error: File '$LOX_FILE' not found"
-    exit 1
+  echo "Error: File '$LOX_FILE' not found"
+  exit 1
 fi
 
 poop \
-    "./zig-out/bin/zlox $LOX_FILE" \
-    "/home/egr/code/rlox/target/release/rlox c $LOX_FILE" \
-    "/home/egr/code/craftinginterpreters/build/clox $LOX_FILE"
+  "/home/egr/code/craftinginterpreters/build/clox $LOX_FILE" \
+  "./zig-out/bin/zlox $LOX_FILE" \
+  "/home/egr/code/rlox/target/release/rlox c $LOX_FILE"
