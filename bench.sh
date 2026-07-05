3b1cffc55d11856d86b6cc422affc65d38003ba5
Alexander Egorov <egoroff@gmail.com>
2026-07-05 12:23:07 +0300

    chore: support multiple benchmark files in bench.sh

--- bench.sh
+++ bench.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # Benchmark script for comparing Lox implementations using poop
-# Usage: ./bench.sh <lox_file>
+# Usage: ./bench.sh <lox_file> [<lox_file> ...]
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <lox_file>"
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <lox_file> [<lox_file> ...]"
     exit 1
 fi
 
-LOX_FILE="$1"
+for LOX_FILE in "$@"; do
+    if [ ! -f "$LOX_FILE" ]; then
+        echo "Error: File '$LOX_FILE' not found"
+        exit 1
+    fi
+done
 
-if [ ! -f "$LOX_FILE" ]; then
-    echo "Error: File '$LOX_FILE' not found"
-    exit 1
-fi
-
-poop \
-    "./zig-out/bin/zlox $LOX_FILE" \
-    "/home/egr/code/craftinginterpreters/build/clox $LOX_FILE" \
-    #"/home/egr/code/rlox/target/release/rlox c $LOX_FILE"
-    
+for LOX_FILE in "$@"; do
+    echo "Benchmarking: $LOX_FILE"
+    echo "----------------------------------------"
+    poop \
+        "./zig-out/bin/zlox $LOX_FILE" \
+        "/home/egr/code/craftinginterpreters/build/clox $LOX_FILE"
+        #"/home/egr/code/rlox/target/release/rlox c $LOX_FILE"
+    echo
+done
