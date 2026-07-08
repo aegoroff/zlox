a45c8668d440258ae32e6a21d78e74f11a32a671
Alexander Egorov <egoroff@gmail.com>
2026-07-08 19:25:27 +0300

    bench: run benchmarks in sorted file order

--- bench_all.sh
+++ bench_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ~/code/craftinginterpreters/test/benchmark -type f -print0 | grep -zEv 'zoo_batch|string_equality' | xargs -0 ./bench.sh
+find ~/code/craftinginterpreters/test/benchmark -type f -print0 | grep -zEv 'zoo_batch|string_equality' | sort -z | xargs -0 ./bench.sh
